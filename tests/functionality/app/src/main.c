329b6fa5342f3d48107ff6bea736cb51059660ef
TylerCGarcia <tyler@garcnet.com>
2025-05-13 10:01:09 -0700

    functionality test structure

---
+++ tests/functionality/app/src/main.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Tests:
+ * - Confirm schedule can be initialized
+ * - multiple actions can be tied to one alarm 
+ * - actions can be scheduled to occur at different times
+ */
+
+#include <zephyr/ztest.h>
+
+LOG_MODULE_REGISTER(tests_app, LOG_LEVEL_DBG);
+
+ZTEST_SUITE(app, NULL, NULL, NULL, NULL, NULL);
+
+ZTEST(app, test_app_init)
+{
+    
+    zassert_true(true, "App init failed");
+}
+
--- tests/functionality/scheduling/src/main.c
+++ tests/functionality/scheduling/src/main.c
@@ -6,10 +6,11 @@
  * - Confirm schedule can be initialized
  * - multiple actions can be tied to one alarm 
  * - actions can be scheduled to occur at different times
- * - 
  */
 
 #include <zephyr/ztest.h>
+#include "sensor_scheduling.h"
+#include "zephyr/logging/log.h"
 
 LOG_MODULE_REGISTER(tests_scheduling, LOG_LEVEL_DBG);
 
@@ -17,6 +18,7 @@ ZTEST_SUITE(scheduling, NULL, NULL, NULL, NULL, NULL);
 
 ZTEST(scheduling, test_scheduling_init)
 {
+    
     zassert_true(true, "Scheduling init failed");
 }
 
